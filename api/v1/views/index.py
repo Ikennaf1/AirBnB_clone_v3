f4a7afa8c77d031a5ce55c541ffc6c494add5bc3
Ikennaf1 <iiikbros202@gmail.com>
2023-04-28 23:50:36 +0100

    Task 3 - get stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,33 @@ Home Base
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    ''' routes to status page '''
+    """
+    routes to status page
+    """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """
+    retrieves the number of each object by type
+    """
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
