6c35e21d11f8ae9cc4694219d865d1e1455423d2
Ikennaf1 <iiikbros202@gmail.com>
2023-04-29 02:06:16 +0100

    Places amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -85,6 +85,7 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
                         storage.save()
         return jsonify({}), 200
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_place_amenity(amenity_id):
     """
